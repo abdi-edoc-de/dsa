377ed9176d5c50967ea97be44157030ca8251420
abdi-edoc-de <61737042+abdi-edoc-de@users.noreply.github.com>
2023-02-08 21:04:30 +0300

    Time: 32 ms (76.84%), Space: 13.9 MB (94.62%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -0,0 +1,26 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        graph = defaultdict(list)
+        for i, (u, v) in enumerate(equations):
+            graph[u].append((v, values[i]))
+            graph[v].append((u, 1 / values[i]))
+            
+        def evaluate(start, target):
+            q = deque([(start, 1)])
+            seen = {start}
+            
+            while q:
+                var, total = q.popleft()
+                
+                if var in graph and var == target:
+                    return total
+                
+                for ngh, val in graph[var]:
+                    if ngh not in seen:
+                        q.append((ngh, total * val))
+                        seen.add(ngh)
+                        
+            return -1
+        
+        for query in queries:
+            yield evaluate(*query)
