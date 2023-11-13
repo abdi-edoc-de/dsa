5691fd65d1b7d34c9794c3293425d26a89b7ce88
Abdulfeta Yusuf <61737042+abdi-edoc-de@users.noreply.github.com>
2023-11-13 15:27:27 +0300

    Time: 65 ms (32.98%), Space: 14.9 MB (22.79%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -3,12 +3,11 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: int
-        """        
+        """
         l = 1
-        for r in range(1, len(nums)):
+        for r in range(1,len(nums)):
             if nums[l-1] != nums[r]:
-                nums[l], nums[r] = nums[r], nums[l]
+                nums[l] = nums[r]
                 l += 1
         return l
-            
-        
+    
