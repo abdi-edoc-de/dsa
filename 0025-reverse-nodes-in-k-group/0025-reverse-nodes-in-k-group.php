c899cdefd0e660d1f7a87306b318a3db522d9d99
Abdulfeta Yusuf <61737042+abdi-edoc-de@users.noreply.github.com>
2023-12-20 04:31:14 +0300

    Time: 14 ms (20.83%), Space: 20.3 MB (79.17%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.php
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.php
@@ -1,43 +1,71 @@
 
 class Solution {
-
-  
     function reverseKGroup($head, $k) {
         if ($head === null || $k === 1) {
             return $head;
         }
 
-        // Store nodes in an array
-        $nodes = [];
-        $current = $head;
-        while ($current !== null) {
-            $nodes[] = $current;
-            $current = $current->next;
-        }
+        $dummy = new ListNode(0);
+        $dummy->next = $head;
+        $prevGroupEnd = $dummy;
 
-        // Reverse groups of k nodes in the array
-        for ($i = 0; $i < count($nodes); $i += $k) {
-            if ($i + $k <= count($nodes)) {
-                $this->reverseArray($nodes, $i, $i + $k - 1);
+        while ($head !== null) {
+            $tail = $head;
+            // Find the k-th node in the group
+            for ($i = 0; $i < $k - 1; $i++) {
+                $tail = $tail->next;
+                if ($tail === null) {
+                    return $dummy->next;  // Less than k nodes remaining, so no more reversals
+                }
             }
+
+            $nextGroupStart = $tail->next;
+            // Reverse the group
+            list($newHead, $newTail) = $this->reverse($head, $tail);
+            // Connect the reversed group with the rest of the list
+            $prevGroupEnd->next = $newHead;
+            $newTail->next = $nextGroupStart;
+
+            $prevGroupEnd = $newTail;
+            $head = $nextGroupStart;
         }
 
-        // Reconstruct the linked list from the nodes array
-        for ($i = 0; $i < count($nodes) - 1; $i++) {
-            $nodes[$i]->next = $nodes[$i + 1];
-        }
-        $nodes[count($nodes) - 1]->next = null;
-
-        return $nodes[0];
+        return $dummy->next;
     }
 
-    private function reverseArray(&$array, $start, $end) {
-        while ($start < $end) {
-            $temp = $array[$start];
-            $array[$start] = $array[$end];
-            $array[$end] = $temp;
-            $start++;
-            $end--;
+    private function reverse($start, $end) {
+        $prev = $end->next;
+        $current = $start;
+
+        while ($prev !== $end) {
+            $temp = $current->next;
+            $current->next = $prev;
+            $prev = $current;
+            $current = $temp;
         }
+
+        return array($end, $start);  // new head, new tail
     }
 }
+
+// Helper function to print the linked list
+function printLinkedList($head) {
+    $current = $head;
+    while ($current !== null) {
+        echo $current->val . ' ';
+        $current = $current->next;
+    }
+    echo "\\n";
+}
+
+// Creating and testing the linked list
+$head = new ListNode(1);
+$current = $head;
+foreach ([2, 3, 4, 5] as $value) {
+    $current->next = new ListNode($value);
+    $current = $current->next;
+}
+
+$solution = new Solution();
+$reversedHead = $solution->reverseKGroup($head, 2);
+printLinkedList($reversedHead);
