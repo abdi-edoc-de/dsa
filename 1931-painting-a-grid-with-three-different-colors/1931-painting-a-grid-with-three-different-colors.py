066f3d62286f2f7320efd86ab49a0a7d7c27c4bd
Abdulfeta Yusuf <61737042+abdi-edoc-de@users.noreply.github.com>
2023-12-30 15:34:43 +0300

    Time: 1352 ms (42.22%), Space: 27.5 MB (75.56%) - LeetHub

---
+++ 1931-painting-a-grid-with-three-different-colors/1931-painting-a-grid-with-three-different-colors.py
@@ -0,0 +1,36 @@
+class Solution:
+    def __init__(self):
+        self.mod = 10**9 + 7
+
+    def generate_mask(self, m, msk, masks, pre_msk, pre_col):
+        if m == 0:
+            masks.append(msk)
+            return
+        for i in range(1, 4):
+            if pre_col != i and ((pre_msk >> ((m - 1) * 2)) & 3) != i:
+                self.generate_mask(m - 1, msk | (i << (m - 1) * 2), masks, pre_msk, i)
+
+    def func(self, m, n, idx, msk, dp, msk_map):
+        if idx == n:
+            return 1
+        if dp[idx][msk] != -1:
+            return dp[idx][msk]
+        if msk not in msk_map:
+            msk_map[msk] = []
+            self.generate_mask(m, 0, msk_map[msk], msk, 0)
+        ans = 0
+        for cur_msk in msk_map[msk]:
+            ans += self.func(m, n, idx + 1, cur_msk, dp, msk_map) * 1
+            ans %= self.mod
+        dp[idx][msk] = ans
+        return ans
+
+    def colorTheGrid(self, m, n):
+        dp = [[-1 for _ in range(1 << (2 * m))] for _ in range(n)]
+        msk_map = {}
+        return self.func(m, n, 0, 0, dp, msk_map)
+
+# Example usage
+# sol = Solution()
+# result = sol.colorTheGrid(3, 3)  # Example arguments
+# print(result)
