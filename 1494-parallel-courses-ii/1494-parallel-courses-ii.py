c596db8dd67fd46890c2c13521582a136a2eff56
Abdulfeta Yusuf <61737042+abdi-edoc-de@users.noreply.github.com>
2023-12-30 17:42:40 +0300

    Time: 1855 ms (17.51%), Space: 18.1 MB (70.06%) - LeetHub

--- 1494-parallel-courses-ii/1494-parallel-courses-ii.py
+++ 1494-parallel-courses-ii/1494-parallel-courses-ii.py
@@ -1,60 +1,57 @@
 class Solution:
     def __init__(self):
-        self.dp = []
+        self.d = []
     
     def minNumberOfSemesters(self, n, relations, k):
-        indegree = [0] * n
-        adj = [[] for _ in range(n)]
+        i = [0] * n
+        a = [[] for _ in range(n)]
 
-        # Counting indegree and storing neighbors
-        for i in relations:
-            indegree[i[1] - 1] += 1
-            adj[i[0] - 1].append(i[1] - 1)
+        for r in relations:
+            i[r[1] - 1] += 1
+            a[r[0] - 1].append(r[1] - 1)
 
-        # Setting all n bits to 1
-        mask = (1 << n) - 1
-        self.dp = [-1] * (mask + 1)
+        m = (1 << n) - 1
+        self.d = [-1] * (m + 1)
 
-        return self.solve(mask, indegree, adj, k)
+        return self.s(m, i, a, k)
     
-    def solve(self, mask, indegree, adj, k):
-        if mask == 0:
+    def s(self, m, i, a, k):
+        if m == 0:
             return 0
-        if self.dp[mask] != -1:
-            return self.dp[mask]
+        if self.d[m] != -1:
+            return self.d[m]
 
-        ans = float('inf')
-        # Get the nodes which are eligible (1 bit) and indegree = 0
-        nodes = [i for i in range(len(indegree)) if mask & (1 << i) and indegree[i] == 0]
-        val = len(nodes)
-        combinations = self.uniqueCombinations(nodes, min(val, k))
+        r = float('inf')
+        n = [j for j in range(len(i)) if m & (1 << j) and i[j] == 0]
+        v = len(n)
+        c = self.u(n, min(v, k))
 
-        for i in combinations:
-            new_mask = mask
-            new_indegree = indegree.copy()
+        for j in c:
+            nm = m
+            ni = i.copy()
 
-            for j in i:
-                new_mask ^= (1 << j)
-                for child in adj[j]:
-                    new_indegree[child] -= 1
+            for x in j:
+                nm ^= (1 << x)
+                for y in a[x]:
+                    ni[y] -= 1
             
-            ans = min(ans, 1 + self.solve(new_mask, new_indegree, adj, k))
+            r = min(r, 1 + self.s(nm, ni, a, k))
 
-        self.dp[mask] = ans
-        return ans
+        self.d[m] = r
+        return r
 
-    def generateCombinations(self, nodes, k, start_index, current_combination, result):
+    def g(self, n, k, s, c, r):
         if k == 0:
-            result.append(current_combination[:])
+            r.append(c[:])
             return
 
-        for i in range(start_index, len(nodes)):
-            current_combination.append(nodes[i])
-            self.generateCombinations(nodes, k - 1, i + 1, current_combination, result)
-            current_combination.pop()
+        for j in range(s, len(n)):
+            c.append(n[j])
+            self.g(n, k - 1, j + 1, c, r)
+            c.pop()
 
-    def uniqueCombinations(self, nodes, k):
-        result = []
-        current_combination = []
-        self.generateCombinations(nodes, k, 0, current_combination, result)
-        return result
+    def u(self, n, k):
+        r = []
+        c = []
+        self.g(n, k, 0, c, r)
+        return r
