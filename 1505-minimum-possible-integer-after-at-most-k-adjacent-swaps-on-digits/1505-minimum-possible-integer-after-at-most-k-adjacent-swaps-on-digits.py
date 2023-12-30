99d5cad933e22a7197dcd1b037f25f596a618f0b
Abdulfeta Yusuf <61737042+abdi-edoc-de@users.noreply.github.com>
2023-12-30 14:41:27 +0300

    Time: 189 ms (86.67%), Space: 19.7 MB (43.33%) - LeetHub

---
+++ 1505-minimum-possible-integer-after-at-most-k-adjacent-swaps-on-digits/1505-minimum-possible-integer-after-at-most-k-adjacent-swaps-on-digits.py
@@ -0,0 +1,54 @@
+class BIT:
+    def __init__(self, n):
+        self.n = n
+        self.nodes = [0] * (n + 1)
+
+    def update(self, i, delta):
+        i += 1
+        while i <= self.n:
+            self.nodes[i] += delta
+            i += i & -i
+
+    def query(self, i):
+        i += 1
+        total = 0
+        while i > 0:
+            total += self.nodes[i]
+            i -= i & -i
+        return total
+
+class Solution:
+    def minInteger(self, num: str, k: int) -> str:
+        qs = [[] for _ in range(10)]
+        n = len(num)
+        
+        for i in range(n):
+            qs[int(num[i])].append(i)
+        
+        lhs = ""
+        removed = [False] * n
+        tree = BIT(n)
+        
+        while k > 0:
+            found = False
+            for d in range(10):
+                if qs[d]:
+                    pos = qs[d][0]
+                    shifted = tree.query(pos - 1)
+                    if pos - shifted <= k:
+                        k -= pos - shifted
+                        tree.update(pos, 1)
+                        qs[d].pop(0)
+                        lhs += str(d)
+                        removed[pos] = True
+                        found = True
+                        break
+            if not found:
+                break
+        
+        rhs = ""
+        for i in range(n):
+            if not removed[i]:
+                rhs += num[i]
+        
+        return lhs + rhs
