440d0f953bf2e201c4e9d0edbfb30e83834072db
Abdulfeta Yusuf <61737042+abdi-edoc-de@users.noreply.github.com>
2024-01-13 12:46:50 +0300

    Time: 442 ms (59.18%), Space: 17.2 MB (44.90%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -1,23 +1,20 @@
 class Solution(object):
     def minimumDeleteSum(self, str1, str2):
-        """
-        :type s1: str
-        :type s2: str
-        :rtype: int
-        """
-        k=len(str1)
-        m=len(str2)
-        dp=[[0 for x in range(m+1)] for x in range(k+1)]
-        for i in range(1,k+1):
-            dp[i][0]=dp[i-1][0]+ord(str1[i-1])
+        k = len(str1)
+        m = len(str2)
+        dp = [[0 for x in range(m + 1)] for x in range(k + 1)]
 
-        for i in range(1,m+1):
-            dp[0][i]=dp[0][i-1]+ord(str2[i-1])
-        for i in range(1,k+1):
-            for j in range(1,m+1):
-                if str1[i-1]==str2[j-1]:
-                    dp[i][j]=dp[i-1][j-1]
+        for i in range(1, k + 1):
+            dp[i][0] = dp[i - 1][0] + ord(str1[i - 1])
+
+        for i in range(1, m + 1):
+            dp[0][i] = dp[0][i - 1] + ord(str2[i - 1])
+
+        for i in range(1, k + 1):
+            for j in range(1, m + 1):
+                if str1[i - 1] == str2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
                 else:
-                    dp[i][j]=min(dp[i-1][j]+ord(str1[i-1]),dp[i][j-1]+ord(str2[j-1]))
+                    dp[i][j] = min(dp[i - 1][j] + ord(str1[i - 1]), dp[i][j - 1] + ord(str2[j - 1]))
 
         return dp[k][m]
